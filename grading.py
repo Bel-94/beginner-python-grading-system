ff040848e1bb99811779080ab4f0071000f39851
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2023-04-03 10:29:04 +0300

    art:delete the previous code and create a new one

--- grading.py
+++ grading.py
@@ -1,29 +1,31 @@
-# Basic Grading System using Python Loops
+# Initialize a variable to hold the total grade
+total_grade = 0
 
-# Ask user for input
-score = int(input("Enter your score: "))
+# Initialize a variable to hold the number of subjects
+num_subjects = 0
 
-# Define grade ranges
-grade_A = range(90, 101)
-grade_B = range(80, 90)
-grade_C = range(70, 80)
-grade_D = range(60, 70)
-grade_F = range(0, 60)
+# Loop through the subjects
+for i in range(1, 6):
+    # Prompt the user for a grade
+    grade = input("Enter the grade for subject {} (0-100): ".format(i))
 
-# Determine the grade based on score input
-if score in grade_A:
-    grade = "A"
-elif score in grade_B:
-    grade = "B"
-elif score in grade_C:
-    grade = "C"
-elif score in grade_D:
-    grade = "D"
-elif score in grade_F:
-    grade = "F"
+    # Check if the input is valid
+    try:
+        grade = float(grade)
+        if grade < 0 or grade > 100:
+            print("Invalid input! Grade should be between 0 and 100.")
+            continue
+    except ValueError:
+        print("Invalid input! Grade should be a number.")
+        continue
+
+    # Add the grade to the total
+    total_grade += grade
+    num_subjects += 1
+
+# Calculate the average grade
+if num_subjects > 0:
+    average_grade = total_grade / num_subjects
+    print("Average grade: {:.2f}".format(average_grade))
 else:
-    print("Invalid input. Score should be between 0 and 100.")
-    exit()
-
-# Print the grade
-print("Your grade is:", grade)
+    print("No valid grades entered.")
